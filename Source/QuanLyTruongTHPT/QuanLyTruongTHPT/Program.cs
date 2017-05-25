c83c0b0cdd58e52e7ac9c46bbe70df8fa67b3e6d
BestThresh <ghostprince.18101996@gmail.com>
2017-05-25 21:24:32 +0700

    Hoang

--- Source/QuanLyTruongTHPT/QuanLyTruongTHPT/Program.cs
+++ Source/QuanLyTruongTHPT/QuanLyTruongTHPT/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using QuanLyTruongTHPT_FORM;
+
 namespace QuanLyTruongTHPT
 {
     static class Program
--- Source/QuanLyTruongTHPT/QuanLyTruongTHPT_BUS/BUS_tblHocSinh.cs
+++ Source/QuanLyTruongTHPT/QuanLyTruongTHPT_BUS/BUS_tblHocSinh.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuanLyTruongTHPT_DAL;
+using QuanLyTruongTHPT_Entity;
+using System.Data;
 
 namespace QuanLyTruongTHPT_BUS
 {
-    class BUS_tblHocSinh
+    public class BUS_tblHocSinh
     {
+        SQL_tblHocsinh bus = new SQL_tblHocsinh();
+        public void addHocsinh(EC_tblHocsinh et)
+        {
+            bus.addHocsinh(et);
+        }
+        //Sua du lieu
+        public void updateHocsinh(EC_tblHocsinh et)
+        {
+            bus.updateHocsinh(et);
+        }
+        //Xoa du lieu
+        public void delHocsinh(EC_tblHocsinh et)
+        {
+            bus.delHocsinh(et);
+        }
+        //select
+        public DataTable getAllHocsinh()
+        {
+            return bus.getAllHocsinh();
+        }
+        public DataTable getHocsinh(string dk)
+        {
+            return bus.getHocsinh(dk);
+        }
+        public DataTable getField(string Field)
+        {
+            return bus.getField(Field);
+        }
+        public DataTable DoDLMaLop(string dk)
+        {
+            return bus.DoDLMaLop(dk);
+        }
+        public DataTable LayMaHS()
+        {
+            return bus.LayRaMaHS();
+        }
     }
 }
